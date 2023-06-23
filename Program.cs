fa8bd83c3b6c1cd1ad1c420f3be40a3479d4e7ff
marvinobig <88102362+marvinobig@users.noreply.github.com>
2023-06-23 08:57:57 +0100

    - update readme - create methods for each maths operation - create method for menu display - loop until correct input is received

--- Program.cs
+++ Program.cs
@@ -2,38 +2,99 @@
 
 Console.Write("What is your name? ");
 string? username = Console.ReadLine();
-char[] availableChoices = new char[5] { 'a', 's', 'm', 'd', 'q' };
+string[] availableChoices = new string[5] { "a", "s", "m", "d", "q" };
 
-Console.WriteLine($"What would you like to study {username}? ");
-Console.WriteLine(@"
-Addition: press the 'a' key
-Substraction: press the 's' key
-Multiplication: press the 'm' key
-Division: press the 'd' key
-Quit: press the 'q' key
-");
+Menu(username, 41);
 
 try
 {
-    char userChoice = Console.ReadKey().KeyChar;
+    string? userChoice;
+
+    do
+    {
+        userChoice = Console.ReadKey().KeyChar.ToString().ToLower();
+
+        if (!availableChoices.Contains(userChoice))
+        {
+            Console.WriteLine("");
+            Console.Write($"{userChoice} is not available, please choose another: ");
+        }
+    }
+    while (!availableChoices.Contains(userChoice));
 
     if (availableChoices.Contains(userChoice))
     {
         Console.WriteLine("");
-        Console.WriteLine($"You have chosen {userChoice}");
-    }
-    else
-    {
-        throw new Exception($"{userChoice} is not an option");
+
+        if (userChoice == "a") Addition("Addition game selected");
+        if (userChoice == "s") Subtraction("Subtraction game selected");
+        if (userChoice == "m") Multiplication("Multiplication game selected");
+        if (userChoice == "d") Division("Division game selected");
+        if (userChoice == "q") Quit("Goodbye");
     }
 }
 catch (Exception err)
 {
     Console.WriteLine("");
     Console.WriteLine(err.Message);
+    Environment.Exit(1);
 }
-
 
 
 Console.WriteLine("Press any key to exit");
 Console.ReadLine();
+
+void Menu(string? username, int seperatorLineLength)
+{
+    Console.WriteLine($"What would you like to study {username}? ");
+
+    for (int i = 0; i < seperatorLineLength; i++)
+    {
+        Console.Write('-');
+    }
+
+    Console.WriteLine("");
+
+    Console.WriteLine(@"
+        Addition: press the 'a' key
+        Substraction: press the 's' key
+        Multiplication: press the 'm' key
+        Division: press the 'd' key
+        Quit: press the 'q' key
+    ");
+
+    for (int i = 0; i < seperatorLineLength; i++)
+    {
+        Console.Write('-');
+    }
+
+    Console.WriteLine("");
+    Console.Write("Game Choice: ");
+}
+
+void Addition(string msg)
+{
+    Random random = new Random();
+    Console.WriteLine(msg);
+}
+
+void Subtraction(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+void Multiplication(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+void Division(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+void Quit(string msg)
+{
+    Console.WriteLine(msg);
+    Environment.Exit(1);
+}
--- README.md
+++ README.md
@@ -11,4 +11,10 @@ Learn maths with this console app.
 - List methods 
 - Branch logic
 - Catching errors using the try...catch statement
+- String methods
+- User defined methods
+- Passing parameters into methods
+- Debugging in Visual Studio
+- do...while loops
+- for loops
 
